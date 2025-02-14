e7ed1afdf17098284124fce2d57e54ea7f014360
Sara <sara.rosa.sr44@gmail.com>
2025-02-14 17:32:47 +0000

    ProjetoTeste

---
+++ ProjetoTeste.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    char CodigoA01 [4];
+
+    printf ("Digite o código da cidade: \n");
+    scanf("%s", CodigoA01);
+    printf("O código da Cidade é: %s\n", CodigoA01);
+return 0;
+}
